0da8c3a3d88e74f8070f1fe47c9e3f05ff627d82
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-10-21 00:46:38 -0300

    second commit

---
+++ scripts/follow.ts
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+// Global config
+const CONFIG = {
+    POLYGON_RPC:"https://polygon-rpc.com/",
+}
+
+
+console.log(process.env.PRIVATE_KEY);
