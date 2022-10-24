4cd2821977035112e13835900939c7abdfbd8fa7
tesslaray <cristian.valdivia@alumnos.usm.cl>
2022-10-24 16:39:54 -0300

    feat: start with login process

--- src/authentication/login.ts
+++ src/authentication/login.ts
@@ -3,15 +3,30 @@ import { lensClient } from "../lens-apollo-client";
 import { GetChallengue } from "../grapql/generated";
 
 import { ChallengueRequest } from "../grapql/lens.types";
+import { getAuthenticationToken } from "../state";
+import { getAddressFromSigner } from "../ethers.service";
 
 const generateChallenge = async (request: ChallengueRequest) => {
-  const variables: ChallengueRequest = {
-    request: { address: "0x3aeC2276326CDC8E9a8A4351c338166e67105AC3" },
-  };
+  const variables: ChallengueRequest = request;
 
   const challengeResponse = await lensClient.request(GetChallengue, variables);
 
   console.log(challengeResponse);
 };
 
-export { generateChallenge };
+const login = async () => {
+  const address = getAddressFromSigner();
+
+  if (getAuthenticationToken()) {
+    console.log("login: Already logged in");
+    return;
+  }
+
+  console.log("login: Login with addres: ", address);
+};
+
+(async () => {
+  await login();
+})();
+
+export { generateChallenge, login };
---
+++ src/state.ts
@@ -0,0 +1,13 @@
+let authenticationToken: string | null = null;
+
+const setAuthenticationToken = (token: string) => {
+  authenticationToken = token;
+
+  console.log("setAuthenticationToken: token", token);
+};
+
+const getAuthenticationToken = () => {
+  return authenticationToken;
+};
+
+export { setAuthenticationToken, getAuthenticationToken };
